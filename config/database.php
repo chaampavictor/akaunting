d5261a6752b506db913b4fdb07d9ee4b11511c17
chaampavictor <32095810+chaampavictor@users.noreply.github.com>
2019-09-30 09:02:57 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -39,13 +39,13 @@ return [
             'prefix' => env('DB_PREFIX', 'ak_'),
         ],
 
-        'mysql' => [
+           'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('us-cdbr-iron-east-05.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('heroku_2611cdbe77b896e'),
+            'username' => env('bd8bb6ee3048ca'),
+            'password' => env('19fbe9a2', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
